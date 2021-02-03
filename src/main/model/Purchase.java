cae21cbc3a8571dda83fef5724473dffb19d8639
Ali <ali.hasan3232@gmail.com>
2021-02-03 00:26:10 -0600

    Some of UI is working - bugs need to be fixed

--- src/main/model/Purchase.java
+++ src/main/model/Purchase.java
@@ -5,15 +5,23 @@ public class Purchase {
     private String name;
     private String type;
     private String date;
-    private double price;
+    private int price;
 
 
-    public Purchase(String date, String type, String name, double price) {
+    public Purchase(String date, String type, String name, int price) {
         this.name = name;
         this.type = type;
         this.date = date;
         this.price = price;
+
     }
+
+    @Override
+    public String toString() {
+        return this.getDate() + ": (" + this.getType() + ") " + this.getName() + " $" + this.getPrice();
+    }
+
+
 
     public String getName() {
         return this.name;
@@ -27,7 +35,7 @@ public class Purchase {
         return this.date;
     }
 
-    public double getPrice() {
+    public int getPrice() {
         return this.price;
     }
 }
